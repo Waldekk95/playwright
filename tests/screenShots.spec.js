d9374b100bc974be29fc7fed5a6c753e707f73d3
Waldekk95 <86876255+Waldekk95@users.noreply.github.com>
2024-03-24 21:56:38 +0100

    adding screenShots example
    adding screenShots example test

--- tests/screenShots.spec.js
+++ tests/screenShots.spec.js
@@ -4,3 +4,8 @@ test("example test", async ({ page }) => {
   await page.goto("https://playwright.dev");
   await expect(page).toHaveScreenshot();
 });
+
+test("example test", async ({ page }) => {
+  await page.goto("https://playwright.dev");
+  await expect(page).toHaveScreenshot({ maxDiffPixels: 100 });
+});
